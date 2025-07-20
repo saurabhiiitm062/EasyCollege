2334e119b733976ab241949f910ad82fe6caaa5d
kanak035 <deepsaurabh4362@gmail.com>
2025-07-20 21:23:47 +0530

    DF

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,33 +2,33 @@ import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
 
-console.log(MONGODB_URI, "loda uri aa rha hai");
+
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-// 👇 Type declaration for globalThis
+// 👇 Type declaration for globalThiss
 declare global {
-  var mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  };
+    var mongoose: {
+        conn: Mongoose | null;
+        promise: Promise<Mongoose> | null;
+    };
 }
 
 // 👇 Setup global cache
 global.mongoose = global.mongoose || { conn: null, promise: null };
 
 export async function connectToDB() {
-  if (global.mongoose.conn) return global.mongoose.conn;
+    if (global.mongoose.conn) return global.mongoose.conn;
 
-  if (!global.mongoose.promise) {
-    global.mongoose.promise = mongoose.connect(MONGODB_URI, {
-      dbName: 'college-system',
-      bufferCommands: false,
-    });
-  }
+    if (!global.mongoose.promise) {
+        global.mongoose.promise = mongoose.connect(MONGODB_URI, {
+            dbName: 'college-system',
+            bufferCommands: false,
+        });
+    }
 
-  global.mongoose.conn = await global.mongoose.promise;
-  return global.mongoose.conn;
+    global.mongoose.conn = await global.mongoose.promise;
+    return global.mongoose.conn;
 }
